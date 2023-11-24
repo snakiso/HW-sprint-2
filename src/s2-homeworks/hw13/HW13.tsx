a72d0148c3bac2dcbd7ac00907cdc2b023d5b3cb
snakiso <igorchagov@mail.ru>
2023-11-24 13:02:05 +0300

    fixed HW13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -37,7 +37,7 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 setInfo('')
-                setText('...всё ок)\n' +
+                setText('...всё ок)' +
                     'код 200 - обычно означает что скорее всего всё ок)')
 
             })
@@ -45,20 +45,18 @@ const HW13 = () => {
                 if (e.response.status === 400) {
                     setCode('Ошибка 400!')
                     setImage(error400)
-                    setText('Ты не отправил success в body вообще!\n' +
+                    setText('Ты не отправил success в body вообще!' +
                         'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
                 }
                 if (e.response.status === 500) {
                     setCode('Ошибка 500!')
                     setImage(error500)
-                    setText('эмитация ошибки на сервере\n' +
-                        'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                    setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных')
                 }
                 if (e.response.status === 0) {
                     setCode('Error!')
                     setImage(errorUnknown)
-                    setText('Network Error\n' +
-                        'AxiosError')
+                    setText('Network Error' + 'AxiosError')
                 }
                 setInfo('')
             })
